8f28a485082dba8fb3b03c079160bac7f973765b
Yu Zhang <atomhust@gmail.com>
2014-10-07 22:10:46 -0700

    tree

--- tree/NumTrees.java
+++ tree/NumTrees.java
@@ -1,7 +1,7 @@
 package Algorithms.tree;
 
 public class NumTrees {
-    public int numTrees(int n) {
+    public int numTrees1(int n) {
         // cnt[n] = cnt[0]cnt[n - 1] + cnt[1]cnt[n - 2] ... cnt[n - 1]cnt[0]
         // cnt[n-1] = cnt[0]cnt[n - 2] + cnt[1]cnt[n - 3] ... cnt[n - 2]cnt[0]
         // For example:
@@ -25,5 +25,20 @@ public class NumTrees {
         }
         
         return cnt[n];
+    }
+    
+    public int numTrees(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        
+        // Get the results of all the trees which 
+        // has the root from 1 to n;
+        int num = 0;
+        for (int i = 0; i <= n - 1; i++) {
+            num += numTrees(i) * numTrees(n - 1 - i);
+        }
+        
+        return num;
     }
 }
