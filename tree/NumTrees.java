32923195d6590ebf18d3871f61a425f57ab61d66
Yu Zhang <atomhust@gmail.com>
2014-10-07 22:10:02 -0700

    tree

---
+++ tree/NumTrees.java
@@ -0,0 +1,29 @@
+package Algorithms.tree;
+
+public class NumTrees {
+    public int numTrees(int n) {
+        // cnt[n] = cnt[0]cnt[n - 1] + cnt[1]cnt[n - 2] ... cnt[n - 1]cnt[0]
+        // cnt[n-1] = cnt[0]cnt[n - 2] + cnt[1]cnt[n - 3] ... cnt[n - 2]cnt[0]
+        // For example:
+        // when N = 3, 
+        // cnt[3] = cnt[0]cnt[2] + cnt[1]cnt[1] + cnt[2]cnt[1];
+        // so the Formula is :
+        
+        // F[n] = ∑ Cnt[j] * cnt[n-j-1]     0<=j<=n-1
+        
+        // base case:
+        // when n = 0, cnt[0] = 1;
+        
+        int[] cnt = new int[n + 1];
+        cnt[0] = 1;
+        
+        for (int i = 1 ; i <= n; i++) {
+            cnt[i] = 0;
+            for (int j = 0; j <= i - 1; j++) {
+                cnt[i] += cnt[j] * cnt[i - j - 1];
+            }
+        }
+        
+        return cnt[n];
+    }
+}
