218d63f6bd55431a219d1c3603df522e8e94b1d4
Yu Zhang <atomhust@gmail.com>
2014-10-09 23:34:11 -0700

    pow

--- Divide2/Pow.java
+++ Divide2/Pow.java
@@ -6,11 +6,28 @@ public class Pow {
             return 0;
         }
         
-        if (n == 0) {
+        if (x == 1 || n == 0) {
             return 1;
         }
         
-        // should consider the case when n is below zero.
+        if (x == -1) {
+            int left = Math.abs(n)%2;
+            if (left == 1) {
+                return -1;
+            } else {
+                return 1;
+            }
+        }
+        
+        // 注意 这样写是不行的，因为-n在n到最小值会出错，
+        // int的最小值（负数）取-n仍然是n 这样就错了。
+        
+        // if (n < 0) {
+        //     double ret1 = pow(x, -n);
+        //     return 1/(double)ret1;
+        // }
+        
+        //should consider the case when n is below zero.
         boolean minus = false;
         if (n < 0) {
             minus = true;
