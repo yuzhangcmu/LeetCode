150f920da0cd4ae6ec8f8f888db294466815f23a
Yu Zhang <atomhust@gmail.com>
2014-10-10 04:05:18 -0700

    tree

---
+++ tree/hasPathSum.java
@@ -0,0 +1,20 @@
+package Algorithms.tree;
+
+public class hasPathSum {
+    /**
+     * Definition for binary tree public class TreeNode { int val; TreeNode
+     * left; TreeNode right; TreeNode(int x) { val = x; } }
+     */
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.left == null && root.right == null && sum == root.val) {
+            return true;
+        }
+
+        return hasPathSum(root.left, sum - root.val)
+                || hasPathSum(root.right, sum - root.val);
+    }
+}
