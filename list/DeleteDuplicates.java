096b7439eb9d191975c45b55e514be99b9956a9a
Yu Zhang <atomhust@gmail.com>
2014-10-09 22:23:40 -0700

    list

---
+++ list/DeleteDuplicates.java
@@ -0,0 +1,40 @@
+package Algorithms.list;
+
+import Algorithms.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class DeleteDuplicates {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        
+        ListNode cur = dummy;
+        
+        while (cur != null) {
+            if (cur.next != null 
+               && cur.next.next != null
+               && cur.next.val == cur.next.next.val) {
+                // remove cur.next;
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;    
+            }
+        }
+        
+        return dummy.next;
+    }
+}
