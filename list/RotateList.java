0060c07292309dc2309a7923c63b2b5b4899740b
Yu Zhang <atomhust@gmail.com>
2014-10-11 00:44:38 -0700

    rotate

---
+++ list/RotateList.java
@@ -0,0 +1,63 @@
+package Algorithms.list;
+
+import Algorithms.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class RotateList {
+    public ListNode rotateRight(ListNode head, int n) {
+        if (head == null) {
+            return null;
+        }
+        
+        // when the list rotate n, it will go back.
+        n = n % getlen(head);
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        
+        // get the pre node before the node which is the new head;
+        // for example: Given 1->2->3->4->5->NULL and k = 2,
+        // we should find 3
+        // return 4->5->1->2->3->NULL.
+        ListNode tail = dummy;
+        while (n > 0) {
+            tail = tail.next;
+            n--;
+        }
+        
+        ListNode pre = dummy;
+        while (tail != null && tail.next != null) {
+            tail = tail.next;
+            pre = pre.next;
+        }
+        
+        // cut the two list and connect the head to the tail.
+        tail.next = dummy.next;
+        
+        ListNode headNew = pre.next;
+        pre.next = null;
+        
+        return headNew;
+    }
+    
+    // get the list lenght.
+    public int getlen(ListNode head) {
+        int len = 0;
+        while (head != null) {
+            len++;
+            head = head.next;
+        }
+        
+        return len;
+    }
+}
