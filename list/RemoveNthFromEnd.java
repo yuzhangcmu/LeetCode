72e5ddfefe06db57fc26bc7644c62aa34581a13b
Yu Zhang <atomhust@gmail.com>
2014-10-11 01:06:34 -0700

    list

---
+++ list/RemoveNthFromEnd.java
@@ -0,0 +1,45 @@
+package Algorithms.list;
+
+import Algorithms.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class RemoveNthFromEnd {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null) {
+            return null;
+        }
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        
+        // move tail N nodes faster than pre.
+        ListNode tail = dummy;
+        // 注意，这里用for比较好一点，用while时，经常不记得将参量--
+        for (int i = n; i > 0; i--) {
+            tail = tail.next;    
+        }
+        
+        ListNode pre = dummy;
+        
+        // get the node before the node we want to delete.
+        while (tail != null && tail.next != null) {
+            tail = tail.next;
+            pre = pre.next;
+        }
+        
+        // DELTE.
+        pre.next = pre.next.next;
+        
+        return dummy.next;
+    }
+}
