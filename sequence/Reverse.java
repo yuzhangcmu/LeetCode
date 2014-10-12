2682dc4e3337e69a4451c4fd074561b6488b32c6
Yu Zhang <atomhust@gmail.com>
2014-10-12 16:36:55 -0700

    reverse

--- sequence/Reverse.java
+++ sequence/Reverse.java
@@ -1,24 +1,19 @@
 package Algorithms.sequence;
 public class Reverse {
-    public int reverse(int x) {
+    public static void main(String[] args) {  
+        System.out.println(reverse(-1234));  
+        System.out.println(reverse(100));  
+    } 
+    
+    public static int reverse(int x) {
         long n = x;
         
-        boolean neg = false;
-        if (n < 0) {
-            neg = true;
-            n = -n;
-        }
-        
         long ret = 0;
-        while (n > 0) {
+        while (n != 0) {
             long left = n % 10;
             ret *= 10;
             ret += left;
             n /= 10;
-        }
-        
-        if (neg) {
-            ret = -ret;
         }
         
         return (int)ret;
