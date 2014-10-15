e7e021d80f8efeffa3ef701cbc54d2d97da4d3ec
Yu Zhang <atomhust@gmail.com>
2014-10-14 17:17:52 -0700

    中心展开

--- algorithm/dp/LongestPalindrome_dp1.java
+++ algorithm/dp/LongestPalindrome_dp1.java
@@ -1,4 +1,4 @@
-package Algorithms.dp;
+package Algorithms.algorithm.dp;
 
 public class LongestPalindrome_dp1 {
     public static void main(String[] args) {
@@ -6,7 +6,7 @@ public class LongestPalindrome_dp1 {
         System.out.println(longestPalindrome(s));
     }
     
-    // solution 1: DP.
+    // Solution 1:
     public static String longestPalindrome1(String s) {
         if (s == null) {
             return null;
@@ -15,7 +15,12 @@ public class LongestPalindrome_dp1 {
         int len = s.length();
 
         // Record i-j is a palindrome.
-        boolean[][] D = new boolean[len][len];
+        boolean[][] D = new int[len][len];
+        for (int i = 0; i < len; i++) {
+        	for (int j = 0; j < len; j++) {
+        		D[i][j] = false;
+        	}
+        }
 
         int max = 0;
         int retB = 0;
@@ -24,26 +29,25 @@ public class LongestPalindrome_dp1 {
         // 因为D[i][j] 要用到i + 1, j - 1，所以每一次计算j时，把j对应的i全部计算完，这样
         // 下一次计算i,j的时候，可以有i+1, j-1可以用。
         for (int j = 0; j < len; j++) {
-            for (int i = 0; i <= j; i++) {
-                if (s.charAt(i) == s.charAt(j)
-                    && (j - i <= 2 || D[i + 1][j - 1])
+        	for (int i = 0; i <= j; i++) {
+        	    if (s.charAt(i) == s.charAt(j)
+                    && (j - i <= 3 || D[i + 1][j - 1])
                     ) {
                     D[i][j] = true;
 
                     if (j - i + 1 > max) {
                         retB = i;
-                        retE = j;
-                        max = j - i + 1;
+                        retE = j;    
                     }
                 } else {
                     D[i][j] = false;
                 }            
-            }
+        	}
         }
         
         return s.substring(retB, retE + 1);
     }
-    
+
     // solution 2: 中心展开法。从头扫到尾部，每一个字符以它为中心向两边扩展，找最长回文。
     // 复杂度为N^2 并且是inplace，空间复杂度O(1)
     public static String longestPalindrome(String s) {
@@ -68,8 +72,7 @@ public class LongestPalindrome_dp1 {
                 max = s1.length();
             }
 
-            // 考虑偶数长度的字符串
-            String s2 = expandAround(s, i, i + 1);
+            String s2 = expandAround(s, i, i);
             if (s2.length() > max) {
                 ret = s2;
                 max = s2.length();
@@ -87,8 +90,8 @@ public class LongestPalindrome_dp1 {
                 break;    
             }
 
-            c1--;
-            c2++;
+            c1++;
+            c2--;
         }
         
         // 注意，根据 substring的定义，c2不要减1
