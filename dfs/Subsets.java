31915c864f0e9797980099750b94bf9c838a6136
Yu Zhang <atomhust@gmail.com>
2014-10-14 22:18:51 -0700

    the subset

---
+++ dfs/Subsets.java
@@ -0,0 +1,40 @@
+package Algorithms.dfs;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Subsets {
+    public List<List<Integer>> subsets(int[] S) {
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        List<Integer> path = new ArrayList<Integer>();
+        
+        if (S == null) {
+            return ret;
+        }
+        
+        Arrays.sort(S);
+        
+        subsets(S, path, ret, 0);
+        
+        return ret;
+    }
+    
+    public void subsets(int[] S, List<Integer> path, List<List<Integer>> ret, int index) {
+        int len = S.length;
+        
+        // 把当前的结果可以添加到结果集中. 空集也算是一种集合 
+        ret.add(new ArrayList<Integer>(path));
+        
+        if (index >= len) {
+            return;
+        }
+        
+        for (int i = index; i < len; i++) {
+            path.add(S[i]);
+            
+            // 注意！这里的index要填写i + 1，而不是index，开始老是会犯错。
+            subsets(S, path, ret, i + 1);
+            path.remove(path.size() - 1);
+        }
+    }
+}
