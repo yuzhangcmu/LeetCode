703db658c556afa9ecdac8858a0dac90a431891e
Yu Zhang <atomhust@gmail.com>
2014-10-17 22:32:56 -0700

    word

---
+++ dp/WordBreak.java
@@ -0,0 +1,39 @@
+package Algorithms.dp;
+
+import java.util.Set;
+
+public class WordBreak {
+    public boolean wordBreak(String s, Set<String> dict) {
+        if (s == null) {
+            return false;
+        }
+        
+        int len = s.length();
+        if (len == 0) {
+            return true;
+        }
+        
+        boolean[] D = new boolean[len + 1];
+
+        // initiate the DP. 注意，这里设置为true是不得已，因为当我们划分字串为左边为0，右边为n的时候，
+        // 而右边的n是一个字典string,那么左边必然要设置为true，才能使结果为true。所以空字符串我们需要
+        // 认为true
+        D[0] = true;
+        
+        // D[i] 表示i长度的字符串能否被word break.
+        for (int i = 1; i <= len; i++) {
+        	// 把子串划分为2部分，分别讨论
+        	D[i] = false;
+        	for (int j = 0; j <= i; j++) {
+        		if (D[j] && dict.contains(s.substring(j, i))) {
+        			// 只要找到任意一个符合条件，我们就可以BREAK; 表示我们检查的
+        			// 这一个子串符合题意
+        			D[i] = true;
+        			break;
+        		}
+        	}
+        }
+
+        return D[len];
+    }
+}
