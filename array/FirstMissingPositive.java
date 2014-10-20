5fadf43f9cfb9f5f884837e91e89ade2c4ff3f8e
Yu Zhang <atomhust@gmail.com>
2014-10-20 03:42:03 -0700

    first

---
+++ array/FirstMissingPositive.java
@@ -0,0 +1,39 @@
+package Algorithms.array;
+
+public class FirstMissingPositive {
+    public static void main(String[] strs) {
+        int[] in = {1,2,0};
+        //int[] in = {3,4,-1,1};        
+        System.out.println(firstMissingPositive(in));
+    }
+    
+    public static int firstMissingPositive(int[] A) {
+        if (A == null) {
+            return 0;
+        }
+        
+        int len = A.length;
+        for (int i = 0; i < len; i++) {
+            // 1. The number should be in the range.
+            // 2. The number should be positive.
+            // 注意：和将要交换的值不可以相同，否则会死循环 
+            while (A[i] <= len && A[i] > 0 && A[A[i] - 1] != A[i]) {
+                swap(A, i, A[i] - 1);
+            }
+        }
+        
+        for (int i = 0; i < len; i++) {
+            if (A[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        
+        return len + 1;
+    }
+    
+    public static void swap(int[] A, int i, int j) {
+        int tmp = A[i];
+        A[i] = A[j];
+        A[j] = tmp;
+    }
+}
