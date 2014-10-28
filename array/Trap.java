9ec2375d57c0133c95753af542790a53bb2e7c5f
Yu Zhang <atomhust@gmail.com>
2014-10-28 02:25:24 -0700

    trap

---
+++ array/Trap.java
@@ -0,0 +1,35 @@
+package Algorithms.array;
+
+public class Trap {
+    public int trap(int[] A) {
+        if (A == null) {
+            return 0;
+        }
+        
+        int max = 0;
+        
+        int len = A.length;
+        int[] left = new int[len];
+        int[] right = new int[len];
+        
+        // count the highest bar from the left to the current.
+        for (int i = 0; i < len; i++) {
+            left[i] = i == 0 ? A[i]: Math.max(left[i - 1], A[i]);
+        }
+        
+        // count the highest bar from right to current.
+        for (int i = len - 1; i >= 0; i--) {
+            right[i] = i == len - 1 ? A[i]: Math.max(right[i + 1], A[i]);
+        }
+        
+        // count the largest water which can contain.
+        for (int i = 0; i < len; i++) {
+            int height = Math.min(right[i], left[i]);
+            if (height > A[i]) {
+                max += height - A[i];
+            }
+        }
+        
+        return max;
+    }
+}
