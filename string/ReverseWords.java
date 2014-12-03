e61df9c33325f9961c75ca34a87fd0a30be421b1
Yu Zhang <atomhust@gmail.com>
2014-12-03 01:07:03 -0800

    2nd sol

--- string/ReverseWords.java
+++ string/ReverseWords.java
@@ -34,5 +34,30 @@ public class ReverseWords {
         
         return sb.toString();
     }
+    
+    /**
+     * @param s : A string
+     * @return : A string
+     */
+    public String reverseWords1(String s) {
+        // write your code
+        if (s == null) {
+            return null;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        // remove the leading and the tail space.
+        String sTrim = s.trim();
+        String strs[] = sTrim.split("\\s+");
+        for (int i = strs.length - 1; i >= 0; i--) {
+            sb.append(strs[i]);
+            if (i != 0) {
+                sb.append(" ");
+            }
+        }
+        
+        return sb.toString();
+    }
 }
 
