c4d9337b461edc0b27e5790171e485444b0368ba
Yu Zhang <atomhust@gmail.com>
2014-12-05 11:52:03 -0800

    dp

--- dp/MinimumTotal.java
+++ dp/MinimumTotal.java
@@ -3,7 +3,10 @@ package Algorithms.dp;
 import java.util.List;
 
 public class MinimumTotal {
-    public int minimumTotal(List<List<Integer>> triangle) {
+    /*
+    REC, SOL 1:
+    */
+    public int minimumTotal1(List<List<Integer>> triangle) {
         if (triangle == null || triangle.size() == 0) {
             return 0;
         }
@@ -33,5 +36,30 @@ public class MinimumTotal {
         }
         
         return mem[row][col];
+    }
+    
+    /*
+    DP, SOL 2:
+    */
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if (triangle == null || triangle.size() == 0) {
+            return 0;
+        }
+        
+        int rows = triangle.size();
+        int[] D = new int[rows];
+        
+        for (int i = rows - 1; i >= 0; i--) {
+            // 注意：边界条件是 j <= i
+            for (int j = 0; j <= i; j++) {
+                if (i == rows - 1) {
+                    D[j] = triangle.get(i).get(j);
+                } else {
+                    D[j] = triangle.get(i).get(j) + Math.min(D[j], D[j + 1]);
+                }    
+            }
+        }
+        
+        return D[0];
     }
 }
