5b410c9d1adb15af7320d9ff8fe70eb03deac36b
Yu Zhang <atomhust@gmail.com>
2014-12-06 17:28:00 -0800

    min cut.

--- dp/MinCut.java
+++ dp/MinCut.java
@@ -7,6 +7,8 @@ public class MinCut {
         }
         
         int len = s.length();
+        
+        // D[i] 的定义: 第i个字符到len - 1个字符的最小切割数。
         int[] D = new int[len];
         
         boolean[][] isPalid = new boolean[len][len];
@@ -21,6 +23,8 @@ public class MinCut {
                 if (s.charAt(i) == s.charAt(j) && (j - i <= 1 || isPalid[i + 1][j - 1])) {
                     isPalid[i][j] = true;
                     if (j == len - 1) {
+                        // 这里就是特殊处理的地方了。当整个字符串都是回文的时候，因为没有D[Len]，所以这里直接
+                        // 将D[i]置为0，意思就是这时不需要任何的划分。
                         D[i] = 0;
                     } else {
                         // 如果前半部分是回文，那么我们可以分解为第一个回文 + 后半部分的最小分割数
