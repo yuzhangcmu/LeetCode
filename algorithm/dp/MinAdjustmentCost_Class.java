41a5c0454b3cf2e564e7e515a6f675676806027f
Yu Zhang <atomhust@gmail.com>
2014-12-09 00:45:29 -0800

    min cost

--- algorithm/dp/MinAdjustmentCost_Class.java
+++ algorithm/dp/MinAdjustmentCost_Class.java
@@ -17,7 +17,7 @@ public class MinAdjustmentCost_Class {
      * @param A: An integer array.
      * @param target: An integer.
      */
-    public static int MinAdjustmentCost(ArrayList<Integer> A, int target) {
+    public static int MinAdjustmentCost1(ArrayList<Integer> A, int target) {
         // write your code here
         if (A == null) {
             return 0;
@@ -26,6 +26,10 @@ public class MinAdjustmentCost_Class {
         return rec(A, new ArrayList<Integer>(A), target, 0);
     }
     
+    /*
+     * SOL 1:
+     * 最普通的递归方法。
+     * */
     public static int rec(ArrayList<Integer> A, ArrayList<Integer> B, int target, int index) {
         int len = A.size();
         if (index >= len) {
@@ -47,6 +51,69 @@ public class MinAdjustmentCost_Class {
             dif = Math.abs(i - A.get(index));
             dif += rec(A, B, target, index + 1);
             min = Math.min(min, dif);
+            
+            // 回溯
+            B.set(index, A.get(index));
+        }
+        
+        return min;
+    }
+    
+    /*
+     * 递归2：
+     * Rec + memory.
+     * */
+    /**
+     * @param A: An integer array.
+     * @param target: An integer.
+     */
+    public static int MinAdjustmentCost(ArrayList<Integer> A, int target) {
+        // write your code here
+        if (A == null || A.size() == 0) {
+            return 0;
+        }
+        
+        int[][] M = new int[A.size()][100];
+        for (int i = 0; i < A.size(); i++) {
+            for (int j = 0; j < 100; j++) {
+                M[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        
+        return rec2(A, new ArrayList<Integer>(A), target, 0, M);
+    }
+    
+    public static int rec2(ArrayList<Integer> A, ArrayList<Integer> B, int target, int index, 
+           int[][] M) {
+        int len = A.size();
+        if (index >= len) {
+            // The index is out of range.
+            return 0;
+        }
+        
+        int dif = 0;
+        int min = Integer.MAX_VALUE;
+        
+        // If this is the first element, it can be from 1 to 100;
+        for (int i = 1; i <= 100; i++) {
+            if (index != 0 && Math.abs(i - B.get(index - 1)) > target) {
+                continue;
+            }
+            
+            if (M[index][i - 1] != Integer.MAX_VALUE) {
+                dif = M[index][i - 1];
+                min = Math.min(min, dif);
+                continue;
+            }
+            
+            B.set(index, i);
+            dif = Math.abs(i - A.get(index));
+            dif += rec2(A, B, target, index + 1, M);
+            
+            min = Math.min(min, dif);
+            
+            // Record the result.
+            M[index][i - 1] = dif;
             
             // 回溯
             B.set(index, A.get(index));
