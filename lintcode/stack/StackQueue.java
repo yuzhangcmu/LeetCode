11a86c5bb34bdeab417cdc4803938366f862abc2
Yu Zhang <atomhust@gmail.com>
2014-12-19 23:34:09 -0800

    queue

---
+++ lintcode/stack/StackQueue.java
@@ -0,0 +1,43 @@
+package Algorithms.lintcode.stack;
+
+import java.util.Stack;
+
+public class StackQueue {
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    public StackQueue() {
+       // do initialization if necessary
+       stack1 = new Stack<Integer>();
+       stack2 = new Stack<Integer>();
+    }
+    
+    public void push(int element) {
+        // write your code here
+        stack1.push(element);
+    }
+
+    public int pop() {
+        // write your code here
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        
+        return stack2.pop();
+    }
+
+    public int top() {
+        // write your code here
+        // write your code here
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        
+        return stack2.peek();
+    }
+}
+
