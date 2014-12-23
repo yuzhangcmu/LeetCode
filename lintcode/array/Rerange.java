7c9bebbaaa0ceb1f2884cb94f1f05bf558f02a0a
Yu Zhang <atomhust@gmail.com>
2014-12-23 03:10:46 -0800

    rerange

--- lintcode/array/Rerange.java
+++ lintcode/array/Rerange.java
@@ -2,7 +2,8 @@ package Algorithms.lintcode.array;
 
 class Rerange {
     public static void main(String[] strs) {
-        int A[] = {1,2,3,4,-1, 2, 5, 5, 5, 6, -7,-8,843,-23,1}; 
+        //int A[] = {1,2,3,4,-1, 2, 5, 5, -5, 6, -7,-8,843,6,1};
+        int A[] = {1,2,-3,-4,-1, -2, -5, -5, -5, 6, -7,-8,843,6,1};
         rerange(A);
 
         for (int i = 0; i < A.length; i++) {
@@ -14,8 +15,8 @@ class Rerange {
      * @param A: An integer array.
      * @return an integer array
      */
-    // SOLUTION 2: 判断正数多还是负数多。 
-    public static int[] rerange(int[] A) {
+    // SOLUTION 1: 判断正数多还是负数多。 
+    public static int[] rerange1(int[] A) {
         // write your code here
         
         // Check the input parameter.
@@ -93,5 +94,103 @@ class Rerange {
        int tmp = A[n1];
        A[n1] = A[n2];
        A[n2] = tmp;
+   }
+   
+   /*
+   Solution 2:
+   */
+   public static int[] rerange(int[] A) {
+        // write your code here
+        
+        // Check the input parameter.
+        if (A == null || A.length <= 2) {
+            return A;
+        }
+        
+        int len = A.length;
+        
+        int cntPositive = 0;
+        
+        for (int num: A) {
+            if (num > 0) {
+                cntPositive++;    
+            }
+        }
+        
+        // If positive numbers are more than negative numbers,
+        // Put the positive numbers at first.
+        int posPointer = 1;
+        int negPointer = 0;
+        
+        // means 
+        boolean pos = false;
+        
+        if (cntPositive > A.length / 2) {
+            // Have more Positive numbers;
+            posPointer = 0;
+            negPointer = 1;
+            
+            pos = true;
+        }
+        
+        int i = 1;
+        int j = len - 2;
+        
+        if (pos) {
+            while (true) {
+                // Put the positive numbers at the end.
+                if (i < len && A[i] < 0) {
+                    i += 2;
+                }
+                
+                if (j > i && A[j] > 0) {
+                    j -= 2;
+                }
+                
+                if (i >= j) {
+                    break;
+                }
+                
+                swap(A, i, j);
+            }
+        } else {
+            while (true) {
+                // Put the negative numbers at the end.
+                if (i < len && A[i] > 0) {
+                    i += 2;
+                }
+                
+                if (j > i && A[j] < 0) {
+                    j -= 2;
+                }
+                
+                if (i >= j) {
+                    break;
+                }
+                
+                swap(A, i, j);
+            }
+        }
+        
+        // Reorder the negative and the positive numbers.
+        while (true) {
+            // Should move if it is in the range.
+            while (posPointer < len && A[posPointer] > 0) {
+                posPointer += 2;
+            }
+            
+            // Should move if it is in the range.
+            while (negPointer < len && A[negPointer] < 0) {
+                negPointer += 2;
+            }
+            
+            if (posPointer >= len || negPointer >= len) {
+                break;
+            }
+            
+            swap(A, posPointer, negPointer);
+        }
+        
+        return A;
    }
 }
