386d1f046fbce83ab852e94db7133cd818b756a0
piggy1991 <bgdt764@vip.qq.com>
2015-01-02 23:00:22 -0800

    long

---
+++ lintcode/dp/LongestCommonSubsequence.java
@@ -0,0 +1,35 @@
+package Algorithms.lintcode.dp;
+
+public class LongestCommonSubsequence {
+    /**
+     * @param A, B: Two strings.
+     * @return: The length of longest common subsequence of A and B.
+     */
+    public int longestCommonSubsequence(String A, String B) {
+        // write your code here
+        if (A == null || B == null) {
+            return 0;
+        }
+        
+        int lenA = A.length();
+        int lenB = B.length();
+        int[][] D = new int[lenA + 1][lenB + 1];
+        
+        for (int i = 0; i <= lenA; i++) {
+            for (int j = 0; j <= lenB; j++) {
+                if (i == 0 || j == 0) {
+                    D[i][j] = 0;
+                } else {
+                    if (A.charAt(i - 1) == B.charAt(j - 1)) {
+                        D[i][j] = D[i - 1][j - 1] + 1;
+                    } else {
+                        D[i][j] = Math.max(D[i - 1][j], D[i][j - 1]);
+                    }
+                }
+            }
+        }
+        
+        return D[lenA][lenB];
+    }
+}
+
