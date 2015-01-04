fa0146982d73ded7eca9283710325e470eac1dcc
piggy1991 <bgdt764@vip.qq.com>
2015-01-04 06:05:14 -0800

    del

--- stack/IsValid.java
+++ stack/IsValid.java
@@ -3,6 +3,37 @@ package Algorithms.stack;
 import java.util.Stack;
 
 public class IsValid {
+	public boolean isValid1(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        
+        Stack<Character> stack = new Stack<Character>();
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            char c = s.charAt(i);
+            if (stack.isEmpty()) {
+                if (c == ']' || c == ')' || c == '}') {
+                    return false;
+                }
+                stack.push(c);
+                continue;
+            }
+            
+            if (c == ')' && stack.peek() == '('
+               || c == ']' && stack.peek() == '['
+               || c == '}' && stack.peek() == '{'
+               ) {
+                stack.pop();
+            } else if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                return false;
+            }
+        }
+        
+        return stack.isEmpty();
+    }
     public boolean isValid(String s) {
         if (s == null) {
             return false;
