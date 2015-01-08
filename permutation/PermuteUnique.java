b8e18ba5b02a75a809958a7c68b4194fdb0fa7bc
Yu Zhang <atomhust@gmail.com>
2015-01-08 01:47:11 -0800

    recover

--- permutation/PermuteUnique.java
+++ permutation/PermuteUnique.java
@@ -4,7 +4,13 @@ import java.util.Arrays;
 import java.util.List;
 
 public class PermuteUnique {
-    public List<List<Integer>> permuteUnique(int[] num) {
+    public static void main(String[] strs) {
+        int[] num = {1, 1, 1, 3};
+        List<List<Integer>> ret = permuteUnique(num);
+        System.out.println(ret.toString());
+    }
+    
+    public static List<List<Integer>> permuteUnique(int[] num) {
         List<List<Integer>> ret = new ArrayList<List<Integer>>();
         if (num == null || num.length == 0) {
             return ret;
@@ -14,12 +20,12 @@ public class PermuteUnique {
         Arrays.sort(num);
         boolean[] visit = new boolean[num.length];
         
-        dfs(num, new ArrayList<Integer>(), ret, visit);
+        dfs1(num, new ArrayList<Integer>(), ret, visit);
         
         return ret;
     }
     
-    public void dfs1(int[] num, ArrayList<Integer> path, List<List<Integer>> ret, boolean[] visit) {
+    public static void dfs1(int[] num, ArrayList<Integer> path, List<List<Integer>> ret, boolean[] visit) {
         int len = num.length;
         if (path.size() == len) {
             ret.add(new ArrayList<Integer>(path));
@@ -30,14 +36,14 @@ public class PermuteUnique {
             // 只能连续地选，这样就可以避免生成重复的solution.
             // 例子：1 2 3 4 4 4 5 6 7 8
             // 444这个的选法只能:4, 44, 444连续这三种选法
-            if (visit[i] || (i != 0 && visit[i - 1] && num[i] == num[i - 1])) {
+            if (visit[i] || (i != 0 && !visit[i - 1] && num[i] == num[i - 1])) {
                 continue;
             }
             
             // 递归以及回溯
             visit[i] = true;
             path.add(num[i]);
-            dfs(num, path, ret, visit);
+            dfs1(num, path, ret, visit);
             path.remove(path.size() - 1);
             visit[i] = false;
         }
