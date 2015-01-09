c8175e1c25913dee7fc45cef51d6f34f418727b5
Jason <liuzhe1218@gmail.com>
2015-01-08 18:53:18 -0800

    all solutions

--- algorithm/interviews/uber/Example.java
+++ algorithm/interviews/uber/Example.java
@@ -240,4 +240,201 @@ public class Example {
     	
     	return max;
     }
+    /* given many logs <username,log_time,logout_time>, output <time,number_of_users> */
+	// use two priorityqueue: O(2nlogn) ; just sort: O(2nlog2n) 
+	public List<result> countRecord(List<record> records){
+		if (records == null || records.size() == 0){
+			return new ArrayList<result>();
+		}
+		List<result> rst = new ArrayList<result>();
+		Collections.sort(records, new Comparator<record>(){
+			@Override
+			public int compare(record o1, record o2) {
+				// TODO Auto-generated method stub
+				return o1.log_time - o2.log_time;
+			}
+		});
+		PriorityQueue<Integer> endheap = new PriorityQueue<Integer>(records.size(),new Comparator<Integer>(){
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				// TODO Auto-generated method stub
+				return o1-o2;
+			}
+		});
+		PriorityQueue<Integer> startheap = new PriorityQueue<Integer>(records.size(),new Comparator<Integer>(){
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				// TODO Auto-generated method stub
+				return o1-o2;
+			}
+		});
+		int curr,i;
+		for (i=0;i<records.size();i++){
+			record tmp = records.get(i);
+			startheap.offer(tmp.log_time);
+			endheap.offer(tmp.logout_time);
+		}
+		// output
+		curr = 0;
+		while (startheap.size() > 0 || endheap.size() > 0){
+			int curr1 = startheap.size() > 0 ? startheap.peek() : -1;
+			int curr2 = endheap.size() > 0 ? endheap.peek() : -1;
+			if (curr1 < 0 || curr1 > curr2){// only end time left
+				while (endheap.size() > 0 && endheap.peek() == curr2){
+					curr--;
+					endheap.poll();
+				}
+				rst.add(new result(curr2,curr));
+			}
+			else if (curr2 < 0 || curr1 < curr2){// go with start time
+				while (startheap.size() > 0 && startheap.peek() == curr1){
+					curr++;
+					startheap.poll();
+				}
+				rst.add(new result(curr1,curr));
+			}
+			else{// curr1 == curr2
+				while (endheap.size() > 0 && endheap.peek() == curr2){
+					curr--;
+					endheap.poll();
+				}
+				while (startheap.size() > 0 && startheap.peek() == curr1){
+					curr++;
+					startheap.poll();
+				}
+				rst.add(new result(curr1,curr));
+			}
+		}
+		
+		return rst;
+	}
+	// spiral matrix
+    public List<Integer> spiralMatrix(int[][] matrix){
+    	if (matrix == null || matrix.length == 0){
+    		return new ArrayList<Integer>();
+    	}
+    	List<Integer> rst = new ArrayList<Integer>();
+    	
+    	int m = matrix.length;
+    	int n = matrix[0].length;
+    	int top = 0;
+    	int bottom = m-1;
+    	int left = 0;
+    	int right = n-1;
+    	int i;
+    	int direction = 0;
+    	while (true){
+    		if (direction == 0){
+    			for (i=left;i<=right;i++){
+    				rst.add(matrix[top][i]);
+    			}
+    			top++;
+    		}
+    		else if (direction == 1){
+    			for (i=top;i<=bottom;i++){
+    				rst.add(matrix[i][right]);
+    			}
+    			right--;
+    		}
+    		else if (direction == 2){
+    			for (i=right;i>=left;i--){
+    				rst.add(matrix[bottom][i]);
+    			}
+    			bottom--;
+    		}
+    		else{
+    			for (i=bottom;i>=top;i--){
+    				rst.add(matrix[i][left]);
+    			}
+    			left++;
+    		}
+    		//break
+    		if (left > right || top > bottom){
+    			break;
+    		}
+    		// switch direction
+    		direction = (direction+1) % 4;
+    	}
+    	
+    	return rst;
+    }
+    // find the index of array in which A[i] = i, sorted array
+    /* problem1: no duplicate inside */
+    public int findIndex(int[] num){
+    	if (num == null || num.length == 0){
+    		return -1; // defined to be the edge case number
+    	}
+    	int left = -1;
+    	int right = num.length;
+    	int mid;
+    	
+    	while (right - left > 1){
+    		mid = left + (right-left)/2;
+    		if (num[mid] == mid){
+    			return mid;
+    		}
+    		else if (num[mid] > mid){// left
+    			right = mid;
+    		}
+    		else{
+    			left = mid;
+    		}
+    	}
+    	return -1;
+    }
+    /* problem2: duplicate exists */
+    public int findIndex2(int[] num){
+    	if (num == null || num.length == 0){
+    		return -1;
+    	}
+    	return findMagic(num,0,num.length-1);
+    }
+    public int findMagic(int[] num, int left, int right){
+    	if (left < 0 || right >= num.length || left > right){
+    		return -1;
+    	}
+    	int mid,curr;
+    	
+    	mid = left + (right-left)/2;
+    	if (num[mid] == mid){
+    		return mid;
+    	}
+    	else{
+    		// optimize here, shorten the length of binary search scope
+    		curr = Math.min(num[mid], mid-1);
+    		int leftIndex = findMagic(num,left,curr);
+    		if (leftIndex >= 0){
+    			return leftIndex;
+    		}
+    		curr = Math.max(mid+1, num[mid]);
+    		int rightIndex = findMagic(num,curr,right);
+    		return rightIndex;
+    	}
+    }
+    /* string shift */
+    public String stringShift(String s,int shift){
+    	if (s == null || s.length() == 0 || shift%26 <= 0){
+    		return s;
+    	}
+    	StringBuilder sb = new StringBuilder();
+    	char curr;
+    	shift = shift%26;
+    	
+    	for (int i=0;i<s.length();i++){
+    		if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z'){
+    			curr = (char)('a'+(s.charAt(i)-'a'+shift)%26);
+    			sb.append(curr);
+    		}
+    		else if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z'){
+    			curr = (char)('A'+(s.charAt(i)-'A'+shift)%26);
+    			sb.append(curr);
+    		}
+    		else{
+    			// throw an exception here!
+    		}
+    	}
+    	
+    	return sb.toString();
+    }
+    
 }
