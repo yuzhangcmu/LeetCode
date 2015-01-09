b41b082c045e1a401423e8cd2672338449c85034
Yu Zhang <atomhust@gmail.com>
2015-01-08 22:33:19 -0800

    bug fix

--- algorithm/interviews/uber/FindEqualIndex.java
+++ algorithm/interviews/uber/FindEqualIndex.java
@@ -6,6 +6,9 @@ public class FindEqualIndex {
     public static void main(String[] str) {
         int[] input = {-4,-3,-2,-1,3,4,6,7,9,10,11,12};
         System.out.println(findEqual(input));
+        
+        int[] input2 = {-4,-3,-2,-1,3,4,6,7,8,9,10,11};
+        System.out.println(findEqual(input2));
     }
 
     public static ArrayList<Integer> findEqual(int[] input) {
@@ -46,7 +49,7 @@ public class FindEqualIndex {
             }
         }
         
-        int rightBound = r == input[l] ? r: l;
+        int rightBound = r == input[r] ? r: l;
         
         for (int i = leftBound; i <= rightBound; i++) {
             ret.add(i);
