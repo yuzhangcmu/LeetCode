a0a0f83bf221dbc5d530d906505fb2dbd83f57b5
Yu Zhang <atomhust@gmail.com>
2015-01-08 22:30:03 -0800

    findEqual

---
+++ algorithm/interviews/uber/FindEqualIndex.java
@@ -0,0 +1,57 @@
+package Algorithms.algorithm.interviews.uber;
+
+import java.util.ArrayList;
+
+public class FindEqualIndex {
+    public static void main(String[] str) {
+        int[] input = {-4,-3,-2,-1,3,4,6,7,9,10,11,12};
+        System.out.println(findEqual(input));
+    }
+
+    public static ArrayList<Integer> findEqual(int[] input) {
+        ArrayList<Integer> ret = new ArrayList<Integer>();
+        
+        int l = 0;
+        int r = input.length - 1;
+        
+        // find the left bound.
+        while (l < r - 1) {
+            int mid = l + (r - l) / 2;
+            
+            // the value >= index.
+            if (input[mid] >= mid) {
+                r = mid;
+            } else {
+                l = mid;
+            }
+        }
+        
+        int leftBound = l == input[l] ? l: r;
+        
+        // bug:should reset l, r.
+        l = 0;
+        r = input.length - 1;
+        
+        // find the right bound.
+        while (l < r - 1) {
+            int mid = l + (r - l) / 2;
+            
+            // the value <= index.
+            if (input[mid] <= mid) {
+                // move right.
+                l = mid;
+            } else {
+                // move left.
+                r = mid;
+            }
+        }
+        
+        int rightBound = r == input[l] ? r: l;
+        
+        for (int i = leftBound; i <= rightBound; i++) {
+            ret.add(i);
+        }
+        
+        return ret;
+    }
+}
--- string/StrStr.java
+++ string/StrStr.java
@@ -7,38 +7,8 @@ public class StrStr {
         System.out.println(strStr("ppa","pa"));
         System.out.println(findPattern("ppa","pax"));
         
-        System.out.println(findAllStrings("xxisxhis"));
+        //System.out.println(findAllStrings("xxisxhis"));
         
-    }
-    
-    public static boolean isAWord(String s) {
-        if (s.equals("is")) {
-            return true;
-        } else if (s.equals("his")){
-            return true;
-        }
-        return false;
-    }
-    
-    public static ArrayList<String> findAllStrings(String s) {
-        ArrayList<String> ret = new ArrayList<String>();
-        
-        if (s == null || s.length() == 0) {
-            return ret;    
-        }
-        
-        int len = s.length();
-        
-        for (int i = 0; i < len; i++) {
-            for (int j = i; j < len; j++) {
-                String sub = s.substring(i, j + 1);
-                if (isAWord(sub)) {
-                    ret.add(sub);
-                }
-            }
-        }
-        
-        return ret;
     }
     
     public static String strStr(String haystack, String needle) {
